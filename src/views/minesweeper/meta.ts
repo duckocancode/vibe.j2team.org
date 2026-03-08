d22859bada3ab28e099a18e10cc549440d3d8b03
Nguyễn Nhật Quang <54244121+Hikaru67@users.noreply.github.com>
2026-03-09 00:04:01 +0700

    feat: add minesweeper (#80)
    * feat(minesweeper): fix eslint
    
    * fix: fix build fail

---
+++ src/views/minesweeper/meta.ts
@@ -0,0 +1,9 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+    name: 'Minesweeper',
+    description: 'Game Dò mìn cổ điển phong cách Retro-Futuristic',
+    author: 'Light',
+}
+
+export default meta
